927a2a272b053fa25d527be288bc1cbb0e242436
gauthierLi <lwklxh@163.com>
2022-03-27 14:39:24 +0800

    coco dataset writing

--- data_loader/coco.py
+++ data_loader/coco.py
@@ -1,4 +1,9 @@
+"""
+    @author: gauthierLi
+    @date:03/07/2022
+"""
 import os
+import cv2
 import pycocotools.coco as coco
 
 from base import BaseDataLoader
@@ -11,10 +16,14 @@ class cocoDataSet(Dataset):
     defult_resolution = [512, 512]
 
     def __init__(self, root_dir, split):
+        """
+            @split: val or train
+        """
         self.anno_path = os.path.join(root_dir, "annotations", 'instances_{}2017.json').format(split)
         self.imgDir_path = os.path.join(root_dir, "{}2017").format(split)
         self._coco = coco.COCO(annotation_file=self.anno_path)
         self._ids = list(sorted(self._coco.imgs.keys()))
+        self.coco_class = dict([(v["id"], v["name"]) for k, v in self._coco.cats.items()])
 
     def __len__(self):
         return len(self._ids)
@@ -22,12 +31,53 @@ class cocoDataSet(Dataset):
     def __getitem__(self, item):
         # get all annotations information of picture
         annos = coco.COCO.getAnnIds(imgIds=self._ids[item])
-        target_dict_list = self._coco.loadAnns(annos)
+        label_info = self._coco.loadAnns(annos)
+        img_Info = self._coco.loadImgs(self._ids[item])
 
-    def _get_format_bbox(self, target_lst):
+        img_abs_path = os.path.join(self.imgDir_path, img_Info[0]["file_name"])
+        img = cv2.imread(img_abs_path)
+        bbox, label = self._get_format_bbox(label_info)
+
+        # gt = self._get_gt(bbox, label)
+        return img, (bbox, label)
+
+
+    def _get_format_bbox(self, label_info):
+        """
+            @label_info: label_info: segmentation information, include bbox, category id
+        """
         bbox_lst = []
-        for item in target_lst:
+        for item in label_info:
+            category_id = item['category_id']
             box = item["bbox"]
-            format_box = (box[0] + box[2] / 2, box[1] + box[3] / 2, box[2] / 2, box[3] / 2)
+            format_box = [(box[0] + box[2] / 2, box[1] + box[3] / 2, box[2] / 2, box[3] / 2), category_id]
             bbox_lst.append(format_box)
         return bbox_lst
+
+    def _get_gt(self, bbox, label):
+        pass
+
+    def draw_bbox(self, img_id, root_path, label_info, text=None):
+        """
+        label_info: segmentation information, include bbox
+        """
+        # img information, file_name
+        img_Info = self._coco.loadImgs(img_id)
+        img_abs_dir = os.path.join(root_path, img_Info[0]["file_name"])
+        img = cv2.imread(img_abs_dir)
+        bbox_lst = self._get_format_bbox(label_info)
+        for (box, cat_id) in bbox_lst:
+            text_info = self.coco_class[cat_id]
+            if text is not None:
+                text_info += text
+            text_len = len(text_info)
+            x1, y1, x2, y2 = [int(box[0] - box[2]), int(box[1] - box[3]), int(box[0] + box[2]), int(box[1] + box[3])]
+            img = cv2.rectangle(img, (x1, y1),
+                                (x2, y2), (255, 0, 0), 1)
+            img = cv2.rectangle(img, (int(box[0] - box[2]), int(box[1] - box[3]) - 16),
+                                (int(box[0] - box[2]) + text_len * 11, int(box[1] - box[3])), (250, 250, 20), -1)
+            img = cv2.putText(img, text_info, (int(box[0] - box[2]), int(box[1] - box[3]) - 5),
+                              cv2.FONT_HERSHEY_COMPLEX,
+                              0.5, (0, 0, 0))
+        cv2.imshow(str(img_Info[0]["id"]), img)
+        cv2.waitKey(0)
--- testArea.py
+++ testArea.py
@@ -4,36 +4,58 @@ import numpy as np
 import pycocotools.coco as coco
 
 
-def _get_format_bbox(target_lst):
+def _get_format_bbox(label_info):
     bbox_lst = []
-    for item in target_lst:
+    for item in label_info:
+        category_id = item['category_id']
         box = item["bbox"]
-        format_box = (box[0] + box[2] / 2, box[1] + box[3] / 2, box[2] / 2, box[3] / 2)
+        format_box = [(box[0] + box[2] / 2, box[1] + box[3] / 2, box[2] / 2, box[3] / 2), category_id]
         bbox_lst.append(format_box)
     return bbox_lst
 
 
-path = f"/mnt/data/coco2017/annotations/instances_val2017.json"
-data = coco.COCO(annotation_file=path)
-ids = list(sorted(data.imgs.keys()))
-
-coco_class = dict([(v["id"], v["name"]) for k, v in data.cats.items()])
-print(coco_class)
-
-for img_id in ids[7:10]:
-    # get annotation info
-    ann_ids = data.getAnnIds(imgIds=img_id)
-    targets = data.loadAnns(ann_ids)
-    print(targets)
-    lst = _get_format_bbox(targets)
-    # print(lst)
+def draw_bbox(img_id, root_path, label_info, text=None):
+    """
+    label_info: segmentation information, include bbox
+    """
+    # img information, file_name
     img_Info = data.loadImgs(img_id)
-    # print(img_Info)
-    img_abs_dir = os.path.join(f"/mnt/data/coco2017/val2017", img_Info[0]["file_name"])
+    img_abs_dir = os.path.join(root_path, img_Info[0]["file_name"])
     img = cv2.imread(img_abs_dir)
-    for box in lst:
-        x1, y1, x2, y2 = int(box[0] - box[2]), int(box[1] - box[3]), int(box[0] + box[2]), int(box[1] + box[3])
+    bbox_lst = _get_format_bbox(label_info)
+    for (box, cat_id) in bbox_lst:
+        text_info = coco_class[cat_id]
+        if text is not None:
+            text_info += text
+        text_len = len(text_info)
+        x1, y1, x2, y2 = [int(box[0] - box[2]), int(box[1] - box[3]), int(box[0] + box[2]), int(box[1] + box[3])]
         img = cv2.rectangle(img, (x1, y1),
                             (x2, y2), (255, 0, 0), 1)
+        img = cv2.rectangle(img, (int(box[0] - box[2]), int(box[1] - box[3]) - 16),
+                            (int(box[0] - box[2]) + text_len * 11, int(box[1] - box[3])), (250, 250, 20), -1)
+        img = cv2.putText(img, text_info, (int(box[0] - box[2]), int(box[1] - box[3]) - 5), cv2.FONT_HERSHEY_COMPLEX,
+                          0.5, (0, 0, 0))
     cv2.imshow(str(img_Info[0]["id"]), img)
     cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    path = f"/mnt/data/coco2017/annotations/instances_val2017.json"
+    data = coco.COCO(annotation_file=path)
+    ids = list(sorted(data.imgs.keys()))
+
+    coco_class = dict([(v["id"], v["name"]) for k, v in data.cats.items()])
+    print(coco_class)
+
+    for img_id in ids[7:10]:
+        # get annotation info
+        ann_ids = data.getAnnIds(imgIds=img_id)
+        print(ann_ids)  # [301431, 559508, 560228, 633972] four targets
+        # segmentation information, include bbox
+        label_info = data.loadAnns(ann_ids)
+        # img information , include filename,'height' and 'width'
+        img_Info = data.loadImgs(img_id)
+
+        bbox_lst = _get_format_bbox(label_info)
+
+        draw_bbox(img_id, f"/mnt/data/coco2017/val2017", label_info)
