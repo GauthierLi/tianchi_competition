eef19a659020a3c5b9063ec0615a9e92f8f183ea
gauthierLi <lwklxh@163.com>
2022-04-28 00:28:26 +0800

    work on GT

--- utils/centerNetGT.py
+++ utils/centerNetGT.py
@@ -3,7 +3,7 @@ import numpy as numpy
 
 class centerNetGT:
     #downsample rate
-    R = 3
+    R = 3.
     def __init__(self, img_size, bboxes):
         self.W, self.H = img_size
         # [[x, y, w/2, h/2, label], ...,]
@@ -11,11 +11,15 @@ class centerNetGT:
         # [50 -- > cls, 51 --> W, 52 --> H, 53 --> offset W, 54 --> offset H]
         self.gt = torch.zeros((54, int(self.W/self.R), int(self.H/self.R)), dtype=torch.float32)
 
+        self.r = 3
+
     def generate_GT(self):
         for box in self.bboxes:
             x, y, w, h, label = box
             label = label - 1
             x_, y_, w_, h_ = int(x/self.R), int(y/self.R), int(w/self.R), int(h/self.R)
+
+            self._caculate_r(w_, h_)
             self.center_gt(label, x_, y_)
             self.WH_gt(x_, y_, w_, h_)
             self.offset_gt(x, y, x_, y_)
@@ -23,14 +27,17 @@ class centerNetGT:
     def center_gt(self, label, x, y):
         self.gt[label][x][y] = 1
 
-
     def WH_gt(self, x, y, w, h):
-        pass
+        for i in range(2 * self.r):
+            for j in range(2 * self.r):
+                self.gt[51][x-self.r+i][y-self.r+j] = w
+                self.gt[52][x-self.r+i][y-self.r+j] = h
 
     def offset_gt(self, x, y, x_, y_):
-        pass
+        self.gt[53][x_][y_] = float(x) / float(self.R) - x_
+        self.gt[54][x_][y_] = float(y) / float(self.R) - y_
 
-    def _caculate_r(self):
+    def _caculate_r(self, w, h, IoU=0.7):
         pass
 
     @property
